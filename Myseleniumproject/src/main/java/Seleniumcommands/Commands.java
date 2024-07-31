25712b3edb42d7aeb2a3c10830a0dbe8f9b755bb
gladi <gladi@LAPTOP-IFRBPTMB>
2024-07-31 14:00:42 +0530

    Task

--- Myseleniumproject/src/main/java/Seleniumcommands/Commands.java
+++ Myseleniumproject/src/main/java/Seleniumcommands/Commands.java
@@ -1,15 +1,41 @@
 package Seleniumcommands;
 
+import java.awt.AWTException;
+import java.awt.Event;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-
 public class Commands {
+public void verifyUser()
+{
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://demowebshop.tricentis.com/");
+	WebElement register=driver.findElement(By.className("ico-register"));
+	register.click();;
+	WebElement gender=driver.findElement(By.id("gender-female"));
+	gender.click();
+	WebElement firstName=driver.findElement(By.id("FirstName"));
+	firstName.sendKeys("Gladis");
+WebElement lastName=driver.findElement(By.id("LastName"));
+lastName.sendKeys("Varghese");
+	WebElement email=driver.findElement(By.id("Email"));
+	email.sendKeys("annavarhese6663@gmail.com");
+	WebElement password=driver.findElement(By.id("Password"));
+	password.sendKeys("addgh1235");
+	WebElement conformpassword=driver.findElement(By.id("ConfirmPassword"));
+	conformpassword.sendKeys("addgh1235");
+	WebElement register1=driver.findElement(By.id("register-button"));
+	register1.click();
+}
 	public void webelementCommand()
 	{
 		
@@ -127,43 +153,83 @@ public class Commands {
 		Actions action=new Actions(driver);
 		action.dragAndDropBy(draganadrop, 150, 200)	.build().perform();	
 	}
+	public void verifyKeyBoadrEvents() throws AWTException
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.qabible.in/select-input.php");
+		Robot robot=new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_T);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_T);
+		driver.close();
+		
+	}
+	public void verifySimpleAlert()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/alerts");
+		WebElement simpleclick=driver.findElement(By.id("alertButton"));
+		simpleclick.click();
+		Alert alert=driver.switchTo().alert();
+		alert.accept();
+	}
+public void verifyConformationAlert()
+{
 
-public void verify_user_registration()
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://demoqa.com/alerts");
+	WebElement conformationAlert=driver.findElement(By.id("confirmButton"));
+	Alert alert=driver.switchTo().alert();
+	String message=alert.getText();
+	System.out.println("Alert msg "+message);
+	alert.accept();
+	WebElement conformationResult=driver.findElement(By.id("confirmResult"));
+	String result=conformationResult.getText();
+	System.out.println("the Result"+result);
+	
+	
+}
+public void verifyPromtAler()
 {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
-	driver.get("https://demowebshop.tricentis.com/");
-	WebElement register=driver.findElement(By.className("ico-register"));
-	register.click();
-	WebElement gender=driver.findElement(By.id("gender-female"));
-	gender.click();
-	WebElement firstName=driver.findElement(By.id("FirstName"));
-	firstName.sendKeys("Gladis");
-WebElement lastName=driver.findElement(By.id("LastName"));
-lastName.sendKeys("Varghese");
-	WebElement email=driver.findElement(By.id("Email"));
-	email.sendKeys("annavarhese6663@gmail.com");
-	WebElement password=driver.findElement(By.id("Password"));
-	password.sendKeys("addgh1235");
-	WebElement conformpassword=driver.findElement(By.id("ConfirmPassword"));
-	conformpassword.sendKeys("addgh1235");
-	WebElement register1=driver.findElement(By.id("register-button"));
-	register1.click();
+	driver.get("https://demoqa.com/alerts");
+	WebElement promtAler=driver.findElement(By.id("promtButton"));
+	promtAler.click();
+	Alert alert=driver.switchTo().alert();
+	alert.sendKeys("hello");
+	alert.accept();
+	WebElement promtMessage=driver.findElement(By.id("promptResult"));
+	String result=promtMessage.getText();
+	System.out.println("message "+result);
+	
+	
 }
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws AWTException {
 		// TODO Auto-generated method stub
 		Commands  obj=new Commands ();
 		//obj.isDisplayed();
 		//obj.isSelected();
 		//obj.isEnabled();
-			//obj.verify_user_registration();
+			//obj.verifyUser();
 		//obj.verifyValueFromDropdown();
 		//obj.getTotalDropDown();
 		//obj.verifyRightClick();
-		obj.verifyDoubleClick();
+		//obj.verifyDoubleClick();
 		//obj.mouseOver();
 		//obj.verifyDragandDrop();
 		//obj.verifyDragandDropusingOffset();
+		//obj.verifyKeyBoadrEvents();
+		//obj.verifySimpleAlert();
+		obj.verifyConformationAlert();
+		//obj.verifyPromtAler();
+		
 	}
 
 }
+
